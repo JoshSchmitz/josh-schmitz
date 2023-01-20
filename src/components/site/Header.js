11352f465d2aa86c57afdf7a76e1f77955d5a063
JoshSchmitz <josh.schmitz1@gmail.com>
2023-01-20 08:03:38 -0600

    created Header.js and _header.scss

--- src/components/site/Header.js
+++ src/components/site/Header.js
@@ -2,20 +2,26 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header works!</h1>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/resume'>Resume</Link>
-        </li>
-        <li>
-          <Link to='/contact'>Contact</Link>
-        </li>
-      </ul>
-    </div>
+    <header>
+      <div className='container'>
+        <div className='logo'>
+          <h1>Josh Schmitz</h1>
+        </div>
+        <div className='menu'>
+          <ul className='menu-nav'>
+            <li className='nav-item current'>
+              <Link to='/'>Home</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/resume'>Resume</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
   );
 };
 
