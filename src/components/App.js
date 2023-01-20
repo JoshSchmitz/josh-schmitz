248ed211b15beca1108a75103a390d2720278155
JoshSchmitz <josh.schmitz1@gmail.com>
2023-01-20 08:05:12 -0600

    Configured Sass setup, added styles in main.scss

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import '../scss/main.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './site/Header';
 import Footer from './site/Footer';
 import Home from './site/Home';
