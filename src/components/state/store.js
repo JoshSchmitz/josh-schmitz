854d094e6f21cef4b5cc092cdbf838b8980ec821
JoshSchmitz <josh.schmitz1@gmail.com>
2023-01-24 15:48:08 -0600

    add experiencesReducer to store.js

--- src/components/state/store.js
+++ src/components/state/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import navReducer from '../nav/state/nav';
+import experiencesReducer from '../experience/state/experiences';
 
 export const store = configureStore({
   reducer: {
     nav: navReducer,
+    experiences: experiencesReducer,
   },
 });
