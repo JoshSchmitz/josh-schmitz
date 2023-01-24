14a5f933cdd7886843a9875c8e559ab1e6e45286
JoshSchmitz <josh.schmitz1@gmail.com>
2023-01-24 15:12:24 -0600

    add grid layout in Resume.js, _resume.scss

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,10 +2,14 @@ import Experiences from '../experience/Experiences';
 
 const Resume = () => {
   return (
-    <article className='page' id='resume'>
-      <h1>Resume works!</h1>
-      <Experiences></Experiences>
-    </article>
+    <div className='page' id='resume'>
+      <article className='businesscard'>
+        <h1>Resume works!</h1>
+      </article>
+      <article>
+        <Experiences></Experiences>
+      </article>
+    </div>
   );
 };
 
