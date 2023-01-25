93cd457e9e8a0274fa57319d04e5014c43c4bd82
JoshSchmitz <josh.schmitz1@gmail.com>
2023-01-25 14:33:44 -0600

    added experiences section in Experiences.js

--- src/components/experience/Experiences.js
+++ src/components/experience/Experiences.js
@@ -1,8 +1,31 @@
+import Experience from './Experience';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteExp } from '../experience/state/experiences';
+
 const Experiences = () => {
+  const { experiences } = useSelector((store) => store.experiences);
+  const dispatch = useDispatch();
+
   return (
-    <div>
-      <h1>Experiences works!</h1>
-    </div>
+    <section className='experiences'>
+      <h1 className='title'>Work Experience</h1>
+      <hr />
+      {experiences.map((exp) => {
+        return (
+          <Experience
+            key={exp.id}
+            position={exp.position}
+            company={exp.company}
+            location={exp.location}
+            startdate={exp.startDate}
+            enddate={exp.endDate}
+            description={exp.description}
+            highlighted={exp.highlighted}
+            // delete={() => dispatch(deleteExp(exp.id))}
+          ></Experience>
+        );
+      })}
+    </section>
   );
 };
 
