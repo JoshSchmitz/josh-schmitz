ba1bbbe5e9e74bc652a2d8014b56ef1e1c7feeaa
JoshSchmitz <josh.schmitz1@gmail.com>
2023-01-26 10:19:20 -0600

    init Education.js

---
+++ src/components/education/Education.js
@@ -0,0 +1,44 @@
+const Education = (props) => {
+  return (
+    <div className={props.highlighted ? 'education highlighted' : 'education'}>
+      <div className='headline'>
+        <h2 className='degree'>
+          {props.degree} in{' '}
+          {props.majors.map((maj, i, { length }) => {
+            if (length - 1 === i) {
+              return maj.title;
+            } else {
+              return `${maj.title} and `;
+            }
+          })}
+        </h2>
+        <div className='actions'>
+          {/* <MdEdit className='action'></MdEdit>
+          <MdDelete className='action' onClick={props.delete}></MdDelete> */}
+        </div>
+        <div className='break'></div>
+        <h3 className='minors'>
+          {props.minors.map((min, i, { length }) => {
+            if (length - 1 === i) {
+              return min.title;
+            } else {
+              return `${min.title} and `;
+            }
+          })}
+        </h3>
+      </div>
+      <div className='details'>
+        <h3 className='insitution'>{props.institution} -</h3>
+        <h3 className='location'>
+          {props.location.city}, {props.location.state}
+        </h3>
+        <div className='break'></div>
+        <h4 className='time'>
+          {props.startdate} to {props.enddate}
+        </h4>
+      </div>
+    </div>
+  );
+};
+
+export default Education;
