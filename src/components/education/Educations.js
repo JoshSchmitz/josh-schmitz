fbc8d5874b406d3d72062500d14c0bb59cbbbd61
JoshSchmitz <josh.schmitz1@gmail.com>
2023-01-26 10:19:49 -0600

    update Educations.js to include Education.js

--- src/components/education/Educations.js
+++ src/components/education/Educations.js
@@ -1,8 +1,28 @@
+import { useSelector } from 'react-redux';
+import Education from './Education';
+
 const Educations = () => {
+  const { educations } = useSelector((store) => store.educations);
+
   return (
     <section className='section educations'>
       <h1 className='title'>Education</h1>
       <hr />
+      {educations.map((ed) => {
+        return (
+          <Education
+            key={ed.id}
+            degree={ed.degree}
+            majors={ed.majors}
+            minors={ed.minors}
+            institution={ed.institution.name}
+            location={ed.institution.location}
+            startdate={ed.startDate}
+            enddate={ed.endDate}
+            highlighted={ed.highlighted}
+          ></Education>
+        );
+      })}
     </section>
   );
 };
