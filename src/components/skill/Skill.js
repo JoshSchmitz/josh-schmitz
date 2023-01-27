74557c9945b969d91611d83db6ca4dbfca8da8e2
JoshSchmitz <josh.schmitz1@gmail.com>
2023-01-27 12:08:02 -0600

    config Skill.js layout, display data from props

--- src/components/skill/Skill.js
+++ src/components/skill/Skill.js
@@ -1,7 +1,38 @@
+import { MdAddTask, MdEdit, MdDelete } from 'react-icons/md';
+
 const Skill = (props) => {
+  const xp = [];
+  for (let i = 0; i < 10; i++) {
+    if (i < props.experience) {
+      xp.push('filled');
+    } else {
+      xp.push('empty');
+    }
+  }
+
   return (
-    <div className='skill'>
-      <h4 className='title'>{props.title}</h4>
+    <div className={props.highlighted ? 'skill highlighted-skill' : 'skill'}>
+      <MdAddTask className='icon'></MdAddTask>
+      <div className='details'>
+        <h4 className='details-name'>
+          {props.title}
+          <span className='details-years'>{props.years} years</span>
+        </h4>
+        <div className='break'></div>
+        <div className='details-exp'>
+          {xp.map((item, i) => {
+            if (item === 'filled') {
+              return <div key={i} className='xp filled'></div>;
+            } else {
+              return <div key={i} className='xp'></div>;
+            }
+          })}
+        </div>
+      </div>
+      {/* <div className='actions'>
+        <MdEdit className='action'></MdEdit>
+        <MdDelete className='action'></MdDelete>
+      </div> */}
     </div>
   );
 };
