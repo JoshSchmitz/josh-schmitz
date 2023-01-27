cd3a7f018a2518016be4654f56eb9c15fd19a33f
JoshSchmitz <josh.schmitz1@gmail.com>
2023-01-27 12:07:28 -0600

    add Skill.js map to Skills.js

--- src/components/skill/Skills.js
+++ src/components/skill/Skills.js
@@ -1,8 +1,26 @@
+import { useSelector } from 'react-redux';
+import Skill from './Skill';
+
 const Skills = () => {
+  const { skills } = useSelector((store) => store.skills);
   return (
-    <div>
-      <h1>Skills Works!</h1>
-    </div>
+    <section className='section skills'>
+      <h1 className='title'>Skills</h1>
+      <hr />
+      <div className='skills-container'>
+        {skills.map((skill) => {
+          return (
+            <Skill
+              key={skill.id}
+              title={skill.title}
+              experience={skill.experience}
+              years={skill.years}
+              highlighted={skill.highlighted}
+            ></Skill>
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
