4edf6ccdb982f801a7f7818755efa6704dacef88
JoshSchmitz <josh.schmitz1@gmail.com>
2023-01-30 16:07:27 -0600

    uptade data structure and content in langages.js

--- src/components/language/state/languages.js
+++ src/components/language/state/languages.js
@@ -1,14 +1,41 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { nanoid } from 'nanoid';
+import {
+  IoLogoJavascript,
+  IoLogoSass,
+  IoLogoHtml5,
+  IoLogoCss3,
+} from 'react-icons/io';
 
-const initalState = {
-  languages: [],
+const initialState = {
+  languages: [
+    {
+      id: nanoid(),
+      title: 'JavaScript',
+      icon: <IoLogoJavascript />,
+    },
+    {
+      id: nanoid(),
+      title: 'HTML',
+      icon: <IoLogoHtml5 />,
+    },
+    {
+      id: nanoid(),
+      title: 'CSS',
+      icon: <IoLogoCss3 />,
+    },
+    {
+      id: nanoid(),
+      title: 'SASS',
+      icon: <IoLogoSass />,
+    },
+  ],
   isloading: true,
 };
 
 const languagesSlice = createSlice({
   name: 'languages',
-  initalState,
+  initialState,
   reducers: {
     deleteLang: (store, action) => {
       const id = action.payload;
