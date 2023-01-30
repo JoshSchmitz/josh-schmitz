c0e02c35c460560e0f37489958157cdeec94b4e4
JoshSchmitz <josh.schmitz1@gmail.com>
2023-01-30 16:07:01 -0600

    update map languages from store.js

--- src/components/language/Languages.js
+++ src/components/language/Languages.js
@@ -1,8 +1,12 @@
+import { useSelector } from 'react-redux';
+
 const Languages = () => {
+  const { languages } = useSelector((store) => store.languages);
   return (
     <section className='section languages'>
       <h1 className='title'>Languages</h1>
       <hr />
+      {languages.map((lang) => lang.icon)}
     </section>
   );
 };
