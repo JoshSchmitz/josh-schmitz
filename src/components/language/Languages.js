372f98c608bc4a23eb13cdcf06ba9a1cc6b29583
JoshSchmitz <josh.schmitz1@gmail.com>
2023-01-31 18:50:23 -0600

    update add wrapper for Languages

--- src/components/language/Languages.js
+++ src/components/language/Languages.js
@@ -1,3 +1,4 @@
+import Language from './Language';
 import { useSelector } from 'react-redux';
 
 const Languages = () => {
@@ -6,7 +7,19 @@ const Languages = () => {
     <section className='section languages'>
       <h1 className='title'>Languages</h1>
       <hr />
-      {languages.map((lang) => lang.icon)}
+      <div className='languages-container'>
+        {languages.map((lang) => {
+          return (
+            <Language
+              key={lang.id}
+              title={lang.title}
+              icon={lang.icon}
+              years={lang.years}
+              highlighted={lang.highlighted}
+            ></Language>
+          );
+        })}
+      </div>
     </section>
   );
 };
