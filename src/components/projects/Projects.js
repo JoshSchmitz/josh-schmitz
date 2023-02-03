3a77a91b40ba11a683b7dd7ef6845f934d449c8c
JoshSchmitz <josh.schmitz1@gmail.com>
2023-02-03 10:19:31 -0600

    init Projects.js

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,11 @@
+const Projects = () => {
+  return (
+    <section className='section projects'>
+      <h1 className='title'>Projects</h1>
+      <hr />
+      <div className='projects-container'></div>
+    </section>
+  );
+};
+
+export default Projects;
