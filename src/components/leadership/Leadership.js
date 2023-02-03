9ea829f074d6800021647828d5838b34904b0ce2
JoshSchmitz <josh.schmitz1@gmail.com>
2023-02-03 09:19:22 -0600

    update configure Leadership.js

--- src/components/leadership/Leadership.js
+++ src/components/leadership/Leadership.js
@@ -1,7 +1,16 @@
 const Leadership = (props) => {
   return (
-    <div>
-      <h1>Leadership works!</h1>
+    <div
+      className={
+        props.highlighted ? 'leadership leadership-highlighted' : 'leadership'
+      }
+    >
+      <h1 className='icon'>{props.icon}</h1>
+      <div className='details'>
+        <h2 className='headline'>{props.title}</h2>
+        <h4 className='date'>{props.date}</h4>
+        <p>{props.description}</p>
+      </div>
     </div>
   );
 };
