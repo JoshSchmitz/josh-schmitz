fc07a67353942ae73de4d23c040b4dfa8bc0bb3c
JoshSchmitz <josh.schmitz1@gmail.com>
2023-02-03 10:19:15 -0600

    init Groups.js

---
+++ src/components/groups/Groups.js
@@ -0,0 +1,11 @@
+const Groups = () => {
+  return (
+    <section className='section groups'>
+      <h1 className='title'>Groups</h1>
+      <hr />
+      <div className='groups-container'></div>
+    </section>
+  );
+};
+
+export default Groups;
