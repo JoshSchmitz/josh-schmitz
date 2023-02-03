0a1fc8da89c1ffa5f45d949bdc2835362f9620fc
JoshSchmitz <josh.schmitz1@gmail.com>
2023-02-03 10:44:38 -0600

    update: map awards from awardsSlice

--- src/components/awards/Awards.js
+++ src/components/awards/Awards.js
@@ -1,9 +1,17 @@
+import { useSelector } from 'react-redux';
+
 const Awards = () => {
+  const { awards } = useSelector((store) => store.awards);
+
   return (
     <section className='section awards'>
       <h1 className='title'>Awards</h1>
       <hr />
-      <div className='awards-container'></div>
+      <div className='awards-container'>
+        {awards.map((award) => {
+          return award.title;
+        })}
+      </div>
     </section>
   );
 };
