41efcee5deada5b199c019ab350ec08d5706ddc0
JoshSchmitz <josh.schmitz1@gmail.com>
2023-02-03 10:19:05 -0600

    init Awards.js

---
+++ src/components/awards/Awards.js
@@ -0,0 +1,11 @@
+const Awards = () => {
+  return (
+    <section className='section awards'>
+      <h1 className='title'>Awards</h1>
+      <hr />
+      <div className='awards-container'></div>
+    </section>
+  );
+};
+
+export default Awards;
