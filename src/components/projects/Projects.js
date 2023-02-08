f3089677581046bc6ae2f38fdc527e517e8b60f0
JoshSchmitz <josh.schmitz1@gmail.com>
2023-02-08 15:09:36 -0600

    update: add props for Project in projects map

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,9 +1,27 @@
+import { useSelector } from 'react-redux';
+import Project from './Project';
+
 const Projects = () => {
+  const { projects } = useSelector((store) => store.projects);
+
   return (
     <section className='section projects'>
       <h1 className='title'>Projects</h1>
       <hr />
-      <div className='projects-container'></div>
+      <div className='projects-container'>
+        {projects.map((proj) => {
+          return (
+            <Project
+              key={proj.id}
+              title={proj.title}
+              start={proj.startDate}
+              end={proj.endDate}
+              description={proj.description}
+              highlighted={proj.highlighted}
+            ></Project>
+          );
+        })}
+      </div>
     </section>
   );
 };
