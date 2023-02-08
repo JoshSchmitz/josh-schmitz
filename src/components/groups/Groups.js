e3b947c13d0cf91120f531e70edd6d643f9459eb
JoshSchmitz <josh.schmitz1@gmail.com>
2023-02-08 09:52:56 -0600

    update: map groups with Group component

--- src/components/groups/Groups.js
+++ src/components/groups/Groups.js
@@ -1,9 +1,18 @@
+import { useSelector } from 'react-redux';
+import Group from './Group';
+
 const Groups = () => {
+  const { groups } = useSelector((store) => store.groups);
+
   return (
     <section className='section groups'>
       <h1 className='title'>Groups</h1>
       <hr />
-      <div className='groups-container'></div>
+      <div className='groups-container'>
+        {groups.map((group) => {
+          return <Group key={group.id} title={group.title}></Group>;
+        })}
+      </div>
     </section>
   );
 };
