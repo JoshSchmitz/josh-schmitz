f8d54d47e578c547c6e885588c08243c37748f64
JoshSchmitz <josh.schmitz1@gmail.com>
2023-02-08 15:08:59 -0600

    update: data structure in projectsSlice

--- src/components/projects/state/projects.js
+++ src/components/projects/state/projects.js
@@ -1,11 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { nanoid } from 'nanoid';
 
+const options = {
+  year: 'numeric',
+  month: 'long',
+};
+
 const initialState = {
   projects: [
     {
       id: nanoid(),
       title: 'Grace Gospel Press Website Redesign',
+      startDate: new Date('2016-01-15').toLocaleDateString(undefined, options),
+      endDate: new Date('2017-09-01').toLocaleDateString(undefined, options),
+      description: `Redesigned the Grace Gospel Press website to parallel the Duluth Bible Church website. Created online bookstore and Grace Family Journal.`,
+      highlighted: false,
+    },
+    {
+      id: nanoid(),
+      title: 'VP Wild Rice Logo Update',
+      startDate: new Date('2008-02-01').toLocaleDateString(undefined, options),
+      endDate: new Date('2008-03-30').toLocaleDateString(undefined, options),
+      description: `Developed a new logo and branding for company and product.`,
+      highlighted: true,
     },
   ],
 };
