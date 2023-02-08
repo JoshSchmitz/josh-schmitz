5a6235d22cad63edaf229bcad729c8b340a7cd2f
JoshSchmitz <josh.schmitz1@gmail.com>
2023-02-08 15:09:54 -0600

    update: display props values

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,7 +1,16 @@
 const Project = (props) => {
   return (
-    <div className='project'>
-      <h4 className='project-title'>{props.title}</h4>
+    <div className={props.highlighted ? 'project highlighted' : 'project'}>
+      <h3 className='project-title'>{props.title}</h3>
+      <h4 className='project-date'>
+        {props.start} to {props.end}
+      </h4>
+      <p className='project-desc'>{props.description}</p>
+      {/* 
+        IDEA: add files to projects
+        -- display images if file is image
+        -- link to file if not image
+      */}
     </div>
   );
 };
