13018ee8b385363e781adc03b145c8e23f787d5a
JoshSchmitz <josh.schmitz1@gmail.com>
2023-02-08 12:07:23 -0600

    update: add startDate, endDate, date conditional rendering

--- src/components/groups/Group.js
+++ src/components/groups/Group.js
@@ -1,7 +1,18 @@
 const Group = (props) => {
+  const options = {
+    year: 'numeric',
+    month: 'long',
+  };
+
   return (
     <div className='group'>
       <h4 className='group-title'>{props.title}</h4>
+      <h4 className='group-date'>
+        {props.start} to{' '}
+        {props.end === new Date().toLocaleDateString(undefined, options)
+          ? 'Now'
+          : props.end}
+      </h4>
     </div>
   );
 };
