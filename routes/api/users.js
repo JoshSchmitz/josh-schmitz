1b2c7b860f8f2edc340c38f4cbd85076546061e0
Josh Schmitz <josh.schmitz1@gmail.com>
2023-11-07 18:21:54 -0600

    add register endpoint in users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,3 +10,39 @@ const validateLoginInput = require('../../validation/login');
 
 // load User model
 const User = require('../../models/user');
+const { log } = require('console');
+
+// @route POST api/users/register
+// @desc Register user
+// @access Public
+router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  // check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exists' });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      // Hash password before saving in database
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch((err) => console.log(err));
+        });
+      });
+    }
+  });
+});
