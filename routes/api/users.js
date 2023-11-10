287c81bc175cd60374d20954ba36e1d1ac17dfce
Josh Schmitz <josh.schmitz1@gmail.com>
2023-11-10 14:33:03 -0600

    add login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -46,3 +46,53 @@ router.post('/register', (req, res) => {
     }
   });
 });
+
+// @route POST api/users/login
+// @desc Register user
+// @access Public
+router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+  // check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // find user by email
+  User.findOne({ email }).then((user) => {
+    // check in user exists
+    if (!user) {
+      return res.status(404).json({ emailnotfound: 'Email not found' });
+    }
+  });
+
+  // check password
+  bcrypt.compare(password, user.password).then((isMatch) => {
+    if (isMatch) {
+      // user matched, create JWT payload
+      const payload = {
+        id: user.id,
+        name: user.name,
+      };
+
+      // sign token
+      jwt.sign(
+        payload,
+        keys.secretOrKey,
+        { expiresIn: 31556926 },
+        (err, token) => {
+          res.json({
+            success: true,
+            token: 'Bearer ' + token,
+          });
+        }
+      );
+    } else {
+      return res.status(400).json({ passwordincorrect: 'Password incorrect' });
+    }
+  });
+});
+
+module.exports = router;
