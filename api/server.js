73c6369db94aa8f28c1ee2a1a79c8a1cdd41b568
Josh Schmitz <josh.schmitz1@gmail.com>
2023-11-10 14:56:32 -0600

    add passport and users route

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('../routes/api/users');
 
 const app = express();
 
@@ -20,6 +23,13 @@ mongoose
   .connect(db)
   .then(() => console.log('MongoDB successfully connected'))
   .catch((err) => console.log(err));
+
+// Passport middleware config
+app.use(passport.initialize());
+require('../config/passport')(passport);
+
+// Routes
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server up and running on port ${port}!`));
