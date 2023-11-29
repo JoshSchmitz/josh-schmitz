3b974e476b261d398a07abed0358d28efd3e037e
Josh Schmitz <josh.schmitz1@gmail.com>
2023-11-29 10:57:20 -0600

    cookieParser middleware

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import userRoutes from './routes/user.js';
 import { notFound, errorHandler } from './middleware/errors.js';
@@ -13,6 +14,7 @@ connectDB();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // routes
 app.use('/api/users', userRoutes);
