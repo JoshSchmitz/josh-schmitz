bdfea96a25ddde4d5174e05816a852ff4763231b
Josh Schmitz <josh.schmitz1@gmail.com>
2023-11-28 19:57:22 -0600

    connect to mongodb

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 // config
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 6000;
+connectDB();
 
 // start server
 app.listen(port, () => console.log(`Server started on port ${port}`));
