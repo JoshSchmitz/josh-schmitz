39953732444fed3803581c97cd41456cac7a3a8c
Josh Schmitz <josh.schmitz1@gmail.com>
2023-12-01 16:41:54 -0600

    header component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Header from './header/Header';
 
 const App = () => {
   return (
     <>
+      <Header></Header>
       <Outlet></Outlet>
     </>
   );
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <div className='container'>
+        <div className='logo'>
+          <h1>Josh Schmitz</h1>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Header;
