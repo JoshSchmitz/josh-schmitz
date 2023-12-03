f2c96515fce4580e18afa5c9a72169e94da792f5
Josh Schmitz <josh.schmitz1@gmail.com>
2023-12-02 19:52:56 -0600

    clean up auth container

--- client/src/components/authenticate/AuthContainer.jsx
+++ client/src/components/authenticate/AuthContainer.jsx
@@ -6,7 +6,6 @@ import Register from './pages/Register';
 const AuthContainer = () => {
   return (
     <div className='auth-container'>
-      <h3>Auth Container</h3>
       <Routes>
         <Route path='/auth/login' element={<Login />} />
         <Route path='/auth/register' element={<Register />} />
