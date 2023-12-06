fd53a59ec50ffc3a340b03cff20c15bd20684b7d
Josh Schmitz <josh.schmitz1@gmail.com>
2023-12-05 21:52:52 -0600

    login form

--- client/src/components/authenticate/pages/Login.jsx
+++ client/src/components/authenticate/pages/Login.jsx
@@ -1,16 +1,51 @@
+import { useState, useDispatch } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h3>Sign In</h3>
-      <div className='form'>
+    <>
+      <div className='form' onSubmit={submitHandler}>
+        <h1 className='title'>Sign In</h1>
+        <h4 className='subtitle'>
+          Sign in to your account using you email address
+        </h4>
         <div className='form-group'>
-          <label htmlFor='email'>Email</label>
+          <input
+            className='input'
+            type='email'
+            placeholder='Email address'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className='input'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <div className='form-group'>
-          <label htmlFor='password'>Password</label>
-        </div>
+        <button className='button' type='submit'>
+          Sign In
+        </button>
       </div>
-    </div>
+      <div className='form-section'>
+        Need an account?{' '}
+        <Link className='link' to='/register'>
+          Register
+        </Link>
+      </div>
+    </>
   );
 };
 export default Login;
