97cbb15d1fc8f9b3b5a2d8e2a1ecc62c1b72f7df
Josh Schmitz <josh.schmitz1@gmail.com>
2023-12-09 19:02:42 -0600

    small media styles

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,15 +1,38 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+
+// import components
 import Authenticate from '../authenticate/Authenticate';
+import MenuButton from './MenuButton';
+
+// import redux state
 import { newCurrentItem } from '../../store/slices/nav';
 
 const Nav = () => {
   const dispatch = useDispatch();
   const { navItems, currentItem } = useSelector((state) => state.nav);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
 
   return (
     <nav className='nav'>
-      <ul className='nav-list'>
+      <div className='nav-list-item-noeffect menu'>
+        <Link
+          to={
+            currentItem === 'Home'
+              ? '/'
+              : `/${currentItem.toString().toLowerCase()}`
+          }
+          onClick={handleOpen}
+        >
+          <MenuButton type='menu' />
+        </Link>
+      </div>
+      <ul className={!open ? 'nav-list' : 'nav-list open'}>
         {navItems.map((item) => {
           const name = item.name;
           return (
@@ -21,17 +44,20 @@ const Nav = () => {
             >
               <Link
                 to={name === 'Home' ? '/' : `/${name.toString().toLowerCase()}`}
-                onClick={() => dispatch(newCurrentItem(name))}
+                onClick={() => {
+                  dispatch(newCurrentItem(name));
+                  open && handleOpen();
+                }}
               >
                 {name}
               </Link>
             </li>
           );
         })}
-        <li className='nav-list-item-noeffect'>
-          <Authenticate></Authenticate>
-        </li>
       </ul>
+      <div className='nav-list-item-noeffect'>
+        <Authenticate />
+      </div>
     </nav>
   );
 };
