247ba336a9b305895ad68169e73dc73970e5375f
Josh Schmitz <josh.schmitz1@gmail.com>
2024-02-08 10:50:56 -0600

    multivalue-text component

---
+++ client/src/components/form/MultivalueText.jsx
@@ -0,0 +1,111 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { useFormContext, Controller } from 'react-hook-form';
+import { motion, AnimatePresence } from 'framer-motion';
+import { isFormInvalid, findInputError, framer_error } from './utils/functions';
+
+// import components
+import { MdError } from 'react-icons/md';
+import CreatableSelect from 'react-select/creatable';
+
+const MultiValueText = ({ id, label, placeholder, validation }) => {
+  const {
+    setValue,
+    control,
+    formState: { errors },
+  } = useFormContext();
+  const inputError = findInputError(errors, id);
+  const isInvalid = isFormInvalid(inputError);
+
+  const [inputValue, setInputValue] = useState('');
+  const [fieldValue, setFieldValue] = useState([]);
+
+  const components = {
+    DropdownIndicator: null,
+  };
+
+  const createOption = (label) => {
+    return { label, value: label };
+  };
+
+  const handleKeyDown = (e) => {
+    if (!inputValue) return;
+    switch (e.key) {
+      case 'Enter':
+      case 'Tab':
+        e.preventDefault();
+        setFieldValue((prev) => [...prev, createOption(inputValue)]);
+        setInputValue('');
+    }
+  };
+
+  useEffect(() => {
+    setValue(id, fieldValue, { shouldValidate: true });
+  }, [id, setValue, fieldValue]);
+
+  return (
+    <div className='selectbox'>
+      {label && (
+        <label className='label' htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <Controller
+        name={id}
+        control={control}
+        rules={validation}
+        render={({ field }) => (
+          <CreatableSelect
+            {...field}
+            id={id}
+            className='select multivaluetext'
+            classNamePrefix='select'
+            unstyled={true}
+            components={components}
+            inputValue={inputValue}
+            isClearable
+            isMulti
+            menuIsOpen={false}
+            onChange={(newValue) => setFieldValue(newValue)}
+            value={fieldValue}
+            onKeyDown={handleKeyDown}
+            onInputChange={(newValue) => setInputValue(newValue)}
+            placeholder={
+              placeholder ? placeholder : 'Type something and press enter...'
+            }
+          />
+        )}
+      />
+      <AnimatePresence mode='wait' initial={false}>
+        {isInvalid && (
+          <InputError
+            type='error'
+            message={inputError.error.message}
+            key={inputError.error.message}
+          />
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+MultiValueText.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  validation: PropTypes.object,
+};
+
+const InputError = ({ message, type }) => {
+  return (
+    <motion.p className={`message ${type}`} {...framer_error}>
+      <MdError />
+      {message}
+    </motion.p>
+  );
+};
+InputError.propTypes = {
+  type: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+};
+
+export default MultiValueText;
