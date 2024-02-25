d2ad3145e740a8320907d1ab6fd6c679d224a40e
Josh Schmitz <josh.schmitz1@gmail.com>
2024-02-24 20:28:27 -0600

    skill categories and skills

---
+++ client/src/components/skill/Skill.jsx
@@ -0,0 +1,154 @@
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+
+// import state
+import { useDeleteSkillMutation } from '../../store/slices/resume/api-skill';
+
+// import components
+import SkillForm from './form/SkillForm';
+import Confirm from '../confirm/Confirm';
+import Modal from 'react-modal';
+
+// import icons
+import { useEffect, useState } from 'react';
+import { MdEdit, MdDelete } from 'react-icons/md';
+import * as BsIcons from 'react-icons/bs';
+import * as DiIcons from 'react-icons/di';
+import * as FaIcons from 'react-icons/fa6';
+import * as IoIcons from 'react-icons/io5';
+
+const Skill = ({ skill, resume }) => {
+  const [experience, setExperience] = useState([]);
+
+  // generate icon
+  let Icon = '';
+  const lib = skill.icon.substring(0, 2);
+  switch (lib) {
+    case 'Bs':
+      Icon = BsIcons[`${skill.icon}`];
+      break;
+    case 'Di':
+      Icon = DiIcons[`${skill.icon}`];
+      break;
+    case 'Fa':
+      Icon = FaIcons[`${skill.icon}`];
+      break;
+    case 'Io':
+      Icon = IoIcons[`${skill.icon}`];
+      break;
+    default:
+      break;
+  }
+
+  // generate experience
+  useEffect(() => {
+    const xp = [];
+    for (let i = 0; i < 6; i++) {
+      if (i < skill.experience) {
+        xp.push('filled');
+      } else {
+        xp.push('empty');
+      }
+    }
+    setExperience(xp);
+  }, [skill]);
+
+  // modal functions
+  const [confirmIsOpen, setConfirmIsOpen] = useState(false);
+  const confirmModal = () => {
+    setConfirmIsOpen(!confirmIsOpen);
+  };
+  const [formIsOpen, setFormIsOpen] = useState(false);
+  const formModal = () => {
+    setFormIsOpen(!formIsOpen);
+  };
+
+  const [deleteSkill, deleteIsLoading] = useDeleteSkillMutation();
+
+  const handleDelete = () => {
+    try {
+      deleteSkill({
+        resumeId: resume,
+        skillId: skill._id,
+      });
+      toast.warning('Skill deleted');
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
+
+  return (
+    <>
+      <Modal
+        className='modal-content'
+        overlayClassName='modal-overlay'
+        contentLabel='Update Education Modal'
+        isOpen={formIsOpen}
+        onRequestClose={formModal}
+        preventScroll={true}
+        shouldFocusAfterRender={false}
+      >
+        <SkillForm
+          resumeId={resume}
+          skillId={skill._id}
+          edit={true}
+          toggleModal={formModal}
+        />
+      </Modal>
+      <Modal
+        className='modal-content'
+        overlayClassName='modal-overlay'
+        contentLabel='Confirm Modal'
+        isOpen={confirmIsOpen}
+        onRequestClose={confirmModal}
+        preventScroll={true}
+        shouldFocusAfterRender={false}
+      >
+        <Confirm
+          confirm={handleDelete}
+          confirmLabel='Delete'
+          loading={deleteIsLoading}
+          close={confirmModal}
+          closeLabel='Cancel'
+          message='Are you sure you want to delete this education?'
+        />
+      </Modal>
+      <div
+        className={skill.highlighted ? 'skill highlighted' : 'skill'}
+        key={skill._id}
+      >
+        <div className='icon'>
+          <Icon />
+        </div>
+        <div className='details'>
+          <div className='info'>
+            <h3 className='name'>{skill.title}</h3>
+            <div className='separator'></div>
+            <p className='years'>
+              {skill.years} {skill.years > 1 ? 'years' : 'year'}
+            </p>
+          </div>
+          <div className='exp'>
+            {experience.map((item, i) => {
+              if (item === 'filled') {
+                return <div key={i} className='xp filled'></div>;
+              } else {
+                return <div key={i} className='xp'></div>;
+              }
+            })}
+          </div>
+        </div>
+        <div className='actions'>
+          <MdEdit className='action update' onClick={formModal} />
+          <MdDelete className='action delete' onClick={confirmModal} />
+        </div>
+      </div>
+    </>
+  );
+};
+Skill.propTypes = {
+  skill: PropTypes.object.isRequired,
+  resume: PropTypes.string.isRequired,
+};
+
+export default Skill;
--- client/src/components/skill/Skills.jsx
+++ client/src/components/skill/Skills.jsx
@@ -1,18 +1,48 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 // import components
 import { MdAddCircleOutline } from 'react-icons/md';
+import RingLoader from 'react-spinners/RingLoader';
 import Modal from 'react-modal';
 import SkillForm from './form/SkillForm';
+import Skill from './Skill';
+
+// import state
+import { useGetSkillQuery } from '../../store/slices/resume/api-skill';
+import { nanoid } from '@reduxjs/toolkit';
 
 const Skills = ({ resumeId }) => {
+  const {
+    data: skills,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetSkillQuery({ resumeId });
+  const [categories, setCategories] = useState([]);
+
   // modal functions
   Modal.setAppElement('#root');
   const [modalIsOpen, setIsOpen] = useState(false);
   const toggleModal = () => {
     setIsOpen(!modalIsOpen);
   };
+
+  useEffect(() => {
+    async function categories() {
+      if (skills) {
+        const cat = [];
+        skills.forEach((sk) => {
+          if (!cat.find((c) => c === sk.category)) {
+            cat.push(sk.category);
+          }
+        });
+        setCategories(cat);
+      }
+    }
+    categories();
+  }, [skills]);
 
   return (
     <>
@@ -27,7 +57,7 @@ const Skills = ({ resumeId }) => {
       >
         <SkillForm resumeId={resumeId} edit={false} toggleModal={toggleModal} />
       </Modal>
-      <section className='section skills'>
+      <section className='section' id='skills'>
         <div className='headline'>
           <h1 className='title'>Skills</h1>
           <div className='actions'>
@@ -38,21 +68,35 @@ const Skills = ({ resumeId }) => {
           </div>
         </div>
         <hr />
-        <div className='skills'>
-          {/* {isLoading && (
-          <RingLoader className='loader-page' loading={isLoading} size={50} />
-        )}
-        {isError && <h1>Error: {error}</h1>}
-        {isSuccess &&
-          eds.map((ed) => {
-            return (
-              <Education
-                key={ed._id}
-                education={ed}
-                resume={resumeId}
-              ></Education>
-            );
-          })} */}
+        <div className='categories'>
+          {isLoading && (
+            <RingLoader className='loader-page' loading={isLoading} size={50} />
+          )}
+          {isError && <h1>Error: {error}</h1>}
+          {isSuccess &&
+            categories.sort().map((cat) => {
+              return (
+                <div key={nanoid()} className='category'>
+                  <h3>{cat}s</h3>
+                  <div className='skills'>
+                    {skills
+                      .filter((skill) => skill.category === cat)
+                      .sort((a, b) => b.experience - a.experience)
+                      .map((skill) => {
+                        return (
+                          skill.category === cat && (
+                            <Skill
+                              key={skill._id}
+                              skill={skill}
+                              resume={resumeId}
+                            />
+                          )
+                        );
+                      })}
+                  </div>
+                </div>
+              );
+            })}
         </div>
       </section>
     </>
