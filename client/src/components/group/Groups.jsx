0171adf25d8216c1517fe2956e4d566efd3a5596
Josh Schmitz <josh.schmitz1@gmail.com>
2024-04-12 21:56:25 -0500

    groups component

---
+++ client/src/components/group/Groups.jsx
@@ -0,0 +1,22 @@
+import { MdAddCircleOutline } from 'react-icons/md';
+
+const Groups = () => {
+  return (
+    <>
+      <section className='section groups'>
+        <div className='headline'>
+          <h1 className='title'>Groups</h1>
+          <div className='actions'>
+            <MdAddCircleOutline
+              className='action create'
+              //   onClick={toggleModal}
+            />
+          </div>
+        </div>
+        <hr />
+        <div className='groups'></div>
+      </section>
+    </>
+  );
+};
+export default Groups;
