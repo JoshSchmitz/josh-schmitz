d81be4574bbd5c632474b2f0d54f33411addd726
Josh Schmitz <josh.schmitz1@gmail.com>
2024-04-13 17:46:47 -0500

    highlighted in award model

--- api/models/award.js
+++ api/models/award.js
@@ -4,6 +4,8 @@ const awardSchema = mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
   date: { type: Date, required: true },
+  icon: { type: String, required: true },
+  highlighted: { type: Boolean, required: true },
 });
 
 const Award = mongoose.model('Award', awardSchema);
