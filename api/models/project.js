1dbe870fd5538eedb3bbdc46d7764981492c6e08
Josh Schmitz <josh.schmitz1@gmail.com>
2024-07-12 20:37:15 -0500

    project model

--- api/models/project.js
+++ api/models/project.js
@@ -4,7 +4,7 @@ const projectSchema = mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   startDate: { type: String, required: true },
-  endDate: { type: String },
+  endDate: String,
   skills: [{ type: mongoose.Schema.Types.ObjectId, required: true }],
   highlighted: { type: Boolean, required: true },
 });
