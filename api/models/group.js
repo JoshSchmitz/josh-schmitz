d7dcd5ec7d273b0959e2179080723fabbe0831c4
Josh Schmitz <josh.schmitz1@gmail.com>
2024-07-12 20:37:08 -0500

    group model

--- api/models/group.js
+++ api/models/group.js
@@ -4,12 +4,12 @@ const groupSchema = mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
   startDate: { type: String, required: true },
-  endDate: { type: String },
+  endDate: String,
   position: [
     {
       title: { type: String, requred: true },
       startDate: { type: String, required: true },
-      endDate: { type: String },
+      endDate: String,
     },
   ],
   highlighted: { type: Boolean, required: true },
