2c96c8e5ee109478740f7f2dac40fcb4ae6f8b64
Josh Schmitz <josh.schmitz1@gmail.com>
2024-09-11 12:23:52 -0500

    Icon component

---
+++ client/src/components/icon/Icon.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// import icons
+import * as MdIcons from 'react-icons/md';
+// import * as AiIcons from 'react-icons/ai';
+// import * as BsIcons from 'react-icons/bs';
+// import * as BiIcons from 'react-icons/bi';
+
+const Icon = ({ icon }) => {
+  const DisplayIcon = (iconName) => {
+    if (iconName.startsWith('Md')) {
+      return MdIcons[iconName];
+    }
+  };
+
+  return <div>{React.createElement(DisplayIcon(icon))}</div>;
+};
+Icon.propTypes = {
+  icon: PropTypes.any,
+};
+
+export default Icon;
