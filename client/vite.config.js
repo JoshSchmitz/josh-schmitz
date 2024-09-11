82a73a7e81d49f1ec6c074a3fbb04ea55d213151
Josh Schmitz <josh.schmitz1@gmail.com>
2024-09-11 12:23:37 -0500

    vite config update

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 7000,
+    port: 7001,
     proxy: {
       '/api': {
         target: 'http://localhost:6000',
